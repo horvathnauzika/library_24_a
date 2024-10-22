59be9b8e552f123a117d3a3b6c4539977908dd8e
horvathnauzika <naihorvath@gmail.com>
2024-10-22 13:51:50 +0200

    utasitasok futtatva + role

--- konyvtar/routes/web.php
+++ konyvtar/routes/web.php
@@ -1,15 +1,9 @@
 <?php
 
-use App\Http\Controllers\FileController;
-use App\Http\Controllers\MailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
-//levélküldés
-Route::get('send-mail', [MailController::class, 'index']);
-//file feltoltes
-Route::get('file-upload', [FileController::class, 'index']);
-Route::post('file-upload', [FileController::class, 'store'])->name('file.store');
 
+require __DIR__.'/auth.php';
